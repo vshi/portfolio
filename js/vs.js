b3d021f20b554250cd4887d178473baff1742227
Victoria Shi <victoria.shi@berkeley.edu>
2015-03-10 01:00:40 -0700

    fix js, working p-item class

--- js/vs.js
+++ js/vs.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-    $("#up").click(function() {
-    $('html, body').animate({
+    
+    $("img[class^='scroll-']").on('click', function(e) {
+    // prevents the default action of the link
+    e.preventDefault();
+    // assigns the text of the clicked-link to a variable for comparison purposes
+    var currentClass = $(this).attr('class');
+    
+    if (currentClass == "scroll-u"){
+        $('html, body').animate({
         scrollTop: $("#header").offset().top
     }, 500);
+    }
+    else if (currentClass == "scroll-r"){
+        
+    }else if (currentClass == "scroll-l"){
+        
+    }else{
+        
+    }
 });
 
 });
