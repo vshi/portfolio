a6cc36172b0a3d9a59e8865604b617cb897160be
Victoria Shi <victoria.shi@berkeley.edu>
2015-03-09 23:29:35 -0700

    add p-caption

--- js/vs.js
+++ js/vs.js
@@ -1,35 +1,9 @@
 $(document).ready(function(){
-    $("#portfolio").owlCarousel({
-        singleItem: true,
-        autoHeight : false,
-        responsive: true,
-        responsiveRefreshRate : 200,
-        responsiveBaseWidth: window
-    });
-        
-    $(".fancybox").fancybox({
-        closeClick : true,
-
-        helpers: {
-              title : {
-                  type : 'inside'
-              }
-          }
-	});
-    
-    $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 700);
-        return false;
-      }
-    }
-  });
+    $("#up").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#header").offset().top
+    }, 500);
 });
+
 });
 
