a085598d26c56a239fd51d94aa788d00c483ad4b
Victoria Shi <victoria.shi@berkeley.edu>
2015-03-14 17:50:50 -0700

    add web design page

--- js/vs.js
+++ js/vs.js
@@ -19,6 +19,24 @@ $(document).ready(function(){
         
     }
 });
+    
+    function isScrolledIntoView(elem) {
+        var centerY = Math.max(0,((jQuery(window).height()-jQuery(elem).outerHeight()) / 2) 
+                      + jQuery(window).scrollTop());
+
+        var elementTop = jQuery(elem).offset().top;
+        var elementBottom = elementTop + jQuery(elem).height();
+
+        return elementTop <= centerY && elementBottom >= centerY;
+    }
+    
+    jQuery(window).on("scroll resize", function() {
+        jQuery(".makeVisible").each(function(index, $(".p-item")) {
+            if (isScrolledIntoView($(".p-item"))) {
+               $(".p-item").animate({opacity: 1.0}, 500);
+            }
+        });
+    });
 
 });
 
